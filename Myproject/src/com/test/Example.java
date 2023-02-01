17c3c754232e472a3d45f11e406d1d97d7b43a7b
Bhagyashri <bhagyashribharad2015@gmail.com>
2023-02-01 19:08:39 +0530

    First version of arraylist is created

---
+++ Myproject/src/com/test/Example.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.ArrayList;
+
+public class Example {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+	       al.add(10);
+	       al.add(20);
+	       System.out.println(al); 
+
+	}
+
+}
